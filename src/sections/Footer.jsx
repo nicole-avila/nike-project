768f138344bb15df4c4873dc208fee961934ca90
eivncia <nicole.a.avila@ericsson.com>
2024-03-10 18:15:26 +0100

    footer...

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,5 @@
-
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return <div className="bg-coral-red h-10 bg-hero">Footer</div>;
+};
 
-export default Footer
+export default Footer;
