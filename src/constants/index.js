60f826e254d167a02773e2d6c1e22ddf7014f86b
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-03-20 23:15:35 +0100

    update constans file

--- src/constants/index.js
+++ src/constants/index.js
@@ -133,8 +133,8 @@ export const footerLinks = [
   {
     title: "Get in touch",
     links: [
-      { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-      { name: "+92554862354", link: "tel:+92554862354" },
+      { name: "customer@nike.com", link: "mailto:nicole-avila@hotmail.com" },
+      { name: "+123456789", link: "mailto:nicole-avila@hotmail.com" },
     ],
   },
 ];
